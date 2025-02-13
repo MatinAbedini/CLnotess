cf854d62f6dcc24476326da9f87e5bb9fe6fe325
Matin Abedini <matin.abedini@gmail.com>
2025-02-13 19:08:40 +0330

    Add user panel menu

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard/", views.UserPanelDashboard, name="user-panel-dashboard-page"),
+    path("", views.UserPanelDashboard.as_view(), name="user-panel-page"),
+    path("edit-account/", views.UserPanelDashboard.as_view(), name="edit-account-page"),
+    path("change-email/", views.UserPanelDashboard.as_view(), name="change-email-page"),
+    path("change-password/", views.UserPanelDashboard.as_view(), name="change-password-page"),
+    path("appearance-settings/", views.UserPanelDashboard.as_view(), name="appearance-settings-page"),
 ]
