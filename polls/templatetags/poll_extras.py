848b473c11d7d38302bf63d8add571fd9a4474a5
Matin Abedini <matin.abedini@gmail.com>
2025-02-14 17:30:01 +0330

    Add split and split_list templatetags

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -1,9 +1,19 @@
 from django import template
 from mimetypes import guess_type
 
+
 register = template.Library()
 
 @register.filter(name="get_file_type")
 def get_file_type(value):
-    """Returns type of the file"""
     return guess_type(value)[0]
+
+
+@register.filter(name="split")
+def split(value, split_by=","):
+    return value.split(split_by)
+
+
+@register.filter(name="split_list")
+def split_list(value, split_by=","):
+    return [split(val, split_by) for val in value]
--- polls/templatetags/poll_extras.pyi
+++ polls/templatetags/poll_extras.pyi
@@ -1,1 +1,19 @@
-def get_file_type(value: str) -> str: ...
+def get_file_type(value: str) -> str:
+    """Returns type of the file"""
+    ...
+
+def split(value: str, split_by: str) -> list[str]:
+    """Splits the string using entered split_by.
+
+    Args:
+        split_by :The character that is going to get used for splitting.
+    """
+    ...
+
+def split_list(value: list[str], split_by: str) -> list[str]:
+    """Splits string in a list using entered split_by.
+
+    Args:
+        split_by :The character that is going to used for splitting . Defaults to ,
+    """
+    ...
