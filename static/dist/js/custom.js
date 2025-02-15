f6fa2e4b9ead80d87a768b74c3dfc73840bc97dd
Matin Abedini <matin.abedini@gmail.com>
2025-02-15 21:48:05 +0330

    Update filtering (JS side)

--- static/dist/js/custom.js
+++ static/dist/js/custom.js
@@ -29,8 +29,7 @@ function filterExams(lessons) {
 
     const selected_statuses = [
         $("#done").prop("checked") ? 1: "",
-        $("#in_progress").prop("checked") ? 2: "",
-        $("#not_done").prop("checked") ? 3: "",
+        $("#not_done").prop("checked") ? 2: "",
     ].filter(Boolean).join(",");
 
     const selected_difficulties= [
@@ -48,14 +47,44 @@ function filterExams(lessons) {
     // And save them in form fields
     // And submit the form
 
-    $("#status").val(selected_statuses);
     $("#difficulty").val(selected_difficulties);
+    $("#status").val(selected_statuses);
     $("#lesson").val(selected_lessons);
     $("#filter_form").submit();
 }
 
+function filterInvitations(lessons) {
+    // Get selected statuses and save them inside a list
+    // Get selected types and save them inside a list
+    // Gets Select lessons and save them inside a list
+
+    const selected_statuses = [
+        $("#done").prop("checked") ? 1: "",
+        $("#in_progress").prop("checked") ? 2: "",
+        $("#not_done").prop("checked") ? 3: "",
+    ].filter(Boolean).join(",");
+
+    const selected_types= [
+        $("#student").prop("checked") ? 1: "",
+        $("#teacher").prop("checked") ? 2: "",
+    ].filter(Boolean).join(",");
+
+    const selected_lessons = lessons
+        .map(lesson => $(`#${lesson}`).prop("checked") ? lesson: "")
+        .filter(Boolean)
+        .join(",");
+
+    // Set statuses, types and lessons, which are going to get filtered,
+    // And save them in form fields
+    // And submit the form
+
+    $("#status").val(selected_statuses);
+    $("#lesson").val(selected_lessons);
+    $("#type").val(selected_types);
+    $("#filter_form").submit();
+}
+
 function fillPage(page) {
-    debugger;
     $("#page").val(page);
     $("#filter_form").submit();
 }
