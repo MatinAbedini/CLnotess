079414451b63778ab24b932dbf584f72161d13e5
Matin Abedini <matin.abedini@gmail.com>
2025-04-21 21:43:44 +0330

    CLnotess v1

--- invitation_module/views.py
+++ invitation_module/views.py
@@ -1,28 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
+from django.http import HttpRequest
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
-from class_module.models import Class, ClassTeacherRole
-from lesson_module.models import Lesson
 from .models import Invitation, InvitationAssignedTo
+from class_module.models import Class
+from lesson_module.models import Lesson
 
 # Create your views here.
 
 
-class InvitationListView(ListView):
+class InvitationListView(LoginRequiredMixin, ListView):
     model = Invitation
     template_name = "invitation_module/invitation-list.html"
     context_object_name = "invitations"
     ordering = "-status"
     paginate_by = 10
-
-    def dispatch(self, request: HttpRequest, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect(reverse("login-page"))
-
-        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -44,29 +39,31 @@ class InvitationListView(ListView):
         status_filter: str = self.request.GET.get("status", "")
         lesson_filter: str = self.request.GET.get("lesson", "")
 
-        base_query = (
-            InvitationAssignedTo.objects.filter(
-                assigned_to=user,
-                is_delete=False
-            ).prefetch_related("invitation")
-        )
+        if not hasattr(self, "base_query"):
+            self.base_query = (
+                InvitationAssignedTo.objects.filter(
+                    assigned_to=user,
+                    is_delete=False
+                )
+                .prefetch_related("invitation")
+            )
 
         # Filter invitations by type, status and lesson field,
         # If user has filter invitations using them
 
         if type_filter != "":
             type_filter = list(map(int, type_filter.split(",")))
-            base_query = base_query.filter(invitation__type__in=type_filter)
+            self.base_query = self.base_query.filter(invitation__type__in=type_filter)
 
         if status_filter != "":
             status_filter = list(map(int, status_filter.split(",")))
-            base_query = base_query.filter(status__in=status_filter)
+            self.base_query = self.base_query.filter(status__in=status_filter)
 
         if lesson_filter != "":
             lesson_filter = lesson_filter.split(",")
-            base_query = base_query.filter(lesson__name__in=lesson_filter)
+            self.base_query = self.base_query.filter(lesson__name__in=lesson_filter)
 
-        return base_query
+        return self.base_query
 
 
 @login_required
@@ -85,9 +82,8 @@ def accept_invitation(request, uuid):
     invite: InvitationAssignedTo = get_object_or_404(
         InvitationAssignedTo.objects.filter(
             assigned_to=user,
-            is_active=True,
             is_delete=False,
-            uuid=uuid
+            uuid=uuid,
         )
         .exclude(status=1)
         .prefetch_related("invitation")
@@ -98,24 +94,15 @@ def accept_invitation(request, uuid):
     # Add user as a student to class, if type is 1
     # Add the user as a teacher to class, if type is 2
 
-    if invite.invitation.type == 1:
-        invite.invitation.assigned_class.assigned_to.add(user)
-    else:
-        update_class: ClassTeacherRole = (
-            ClassTeacherRole(
-                teacher=user,
-                assigned_class=invite.invitation.assigned_class,
-                lesson=invite.lesson
-            )
-        )
-
-        update_class.save()
+    if invite.status == 3 and invite.invitation.type == 2:
+        invite.invitation.assigned_class.teacher.add(user)
 
 
     # Change the status of invite
     # Redirect the user to invitations list page
 
     invite.status = 1
+    invite.invitation.assigned_class.assigned_to.add(user)
     invite.save()
 
     return redirect(reverse("invitation-list-page"))
@@ -136,47 +123,33 @@ def reject_invitation(request, uuid):
     invite: InvitationAssignedTo = get_object_or_404(
         InvitationAssignedTo.objects.filter(
             assigned_to=user,
-            is_active=True,
             is_delete=False,
-            uuid=uuid
+            uuid=uuid,
         )
         .exclude(status=2)
         .only("status")
     )
 
+    assigned_class = (
+        Class.objects.get(
+            assigned_to=user,
+            is_active=True,
+            is_delete=False
+        )
+    )
+
     # Remove the user from assigned class
 
-    if invite.status == 1:
-        assigned_class: Class = (
-            Class.objects.filter(
-                assigned_to=user,
-                is_active=True,
-                is_delete=False
-            )
-            .first()
-        )
-        assigned_class.assigned_to.remove(user)
-        assigned_class.save()
-
-        if invite.invitation.type == 2:
-            assigned_teacher_role: ClassTeacherRole = (
-                ClassTeacherRole.objects.filter(
-                    teacher=user,
-                    for_class=assigned_class,
-                    is_active=True,
-                    is_delete=False
-                )
-                .first()
-            )
-
-            assigned_class.is_delete = True
-            assigned_teacher_role.save()
-
+    if invite.status == 3 and invite.invitation.type == 2:
+        assigned_class.teacher.remove(user)
 
     # Change the status of invite
     # Redirect the user to invitations list page
 
+    assigned_class.assigned_to.remove(user)
     invite.status = 2
+
+    assigned_class.save()
     invite.save()
 
     return redirect(reverse("invitation-list-page"))
