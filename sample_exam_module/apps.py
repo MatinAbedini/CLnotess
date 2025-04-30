2d8afbef060ccbfdd9c89980015257e588776627
Matin Abedini <matin.abedini@gmail.com>
2025-04-30 20:33:35 +0330

    Add: sample_exam_module admin

--- sample_exam_module/apps.py
+++ sample_exam_module/apps.py
@@ -1,6 +1,8 @@
+from django.utils.translation import gettext_lazy as _
 from django.apps import AppConfig
 
 
 class SampleExamModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sample_exam_module'
+    verbose_name = _('اپلیکیشن نمونه سوالات امتحانی')
